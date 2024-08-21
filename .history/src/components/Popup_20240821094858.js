e8f3b39c435633260ce7cc898ce89fa550193c68
sibusiso <sibusisok59@gmail.com>
2024-08-21 10:07:15 +0200

    update

---
+++ .history/src/components/Popup_20240821094858.js
@@ -0,0 +1,47 @@
+import React from 'react';
+impor
+
+
+function Popup({ isOpen, onClose, formData, handleChange, handleSubmit, editingId }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className='popup-overlay' onClick={onClose}>
+      <div className='container' onClick={(e) => e.stopPropagation()}>
+        <div className="card">
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              name="name"
+              className="value" 
+              placeholder="Item Name" 
+              value={formData.name}
+              onChange={handleChange} 
+            />
+            <input 
+              type="text" 
+              name="category"
+              className="category" 
+              placeholder="Category" 
+              value={formData.category}
+              onChange={handleChange} 
+            />
+            <input 
+              type="number" 
+              name="quantity"
+              placeholder="Quantity" 
+              value={formData.quantity}
+              onChange={handleChange}
+              min="1" 
+            />
+            <button type="submit" className="sav">
+              {editingId ? "Update" : "Save"}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Popup;
